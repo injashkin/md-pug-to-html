f2440b4ed04106d71b90ac49c9818d48a692a149
injashkin <injashkin@gmail.com>
2022-08-06 02:19:53 +0300

    Bugfix

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const mdToData = require('./md-to-data');
 
-const args = {};
-let sourceDir, destinationDir, templateDir, dataOutDir;
+function mdPugToHtml() {
+  const args = {};
+  let sourceDir, destinationDir, templateDir, dataOutDir;
 
-process.argv.slice(2).forEach((option) => {
-  var values = option.split('=');
-  var key = values.shift();
-  args[key] = (args[key] || []).concat(values);
-});
+  process.argv.slice(2).forEach((option) => {
+    var values = option.split('=');
+    var key = values.shift();
+    args[key] = (args[key] || []).concat(values);
+  });
 
-if (args['-i']) {
-  sourceDir = `${args['-i']}`; // из какого каталога нужно получить файлы .md
-} else console.log('ОШИБКА: Не задан каталог источника с ключом -i=');
+  if (args['-i']) {
+    sourceDir = `${args['-i']}`; // из какого каталога нужно получить файлы .md
+  } else console.log('ОШИБКА: Не задан каталог источника с ключом -i=');
 
-if (args['-o']) {
-  destinationDir = `${args['-o']}`; // каталог сборки проекта
-} else destinationDir = 'docs';
+  if (args['-o']) {
+    destinationDir = `${args['-o']}`; // каталог сборки проекта
+  } else destinationDir = 'docs';
 
-if (args['-t']) {
-  templateDir = `${args['-t']}`; // каталог шаблона статьи
-} else console.log('ОШИБКА: Не задан каталог шаблона статьи с ключом -o=');
+  if (args['-t']) {
+    templateDir = `${args['-t']}`; // каталог шаблона статьи
+  } else console.log('ОШИБКА: Не задан каталог шаблона статьи с ключом -o=');
 
-if (args['-d']) {
-  dataOutDir = `${args['-d']}`; // каталог вывода файла данных
-} else dataOutDir = 'src/data';
+  if (args['-d']) {
+    dataOutDir = `${args['-d']}`; // каталог вывода файла данных
+  } else dataOutDir = 'src/data';
 
-mdToData.separateMd(sourceDir, destinationDir, templateDir, dataOutDir);
+  mdToData.separateMd(sourceDir, destinationDir, templateDir, dataOutDir);
+}
 
+module.exports = { mdPugToHtml };
+mdPugToHtml();
