29ffda7883f2b0c77f016e02355b3955aeb67e3b
injashkin <injashkin@gmail.com>
2022-09-15 10:29:18 +0300

    Refactor tests v.2.2.2

--- test/index.test.js
+++ test/index.test.js
@@ -4,8 +4,6 @@ const fs = require('fs');
 
 const { init, getDataList } = require('../index');
 
-fs.rmSync('test/in-out/dest', { recursive: true, force: true });
-
 const options = {
   sourceDir: 'test/in-out/content',
   destinationDir: 'test/in-out/dest',
@@ -13,9 +11,20 @@ const options = {
   dataOutDir: 'test/in-out/dest',
 };
 
+beforeEach(() => {
+  fs.rmSync(options.destinationDir, { recursive: true, force: true });
+});
+
 describe('init module', () => {
+  test('No options', () => {
+    expect(() => {
+      expect(init());
+    }).toThrow(
+      'ERROR: The directory from which the .md files can be retrieved is not specified'
+    );
+  });
+
   test('sourceDir option', () => {
-    fs.rmSync(options.destinationDir, { recursive: true, force: true });
     options.sourceDir = undefined;
     expect(() => {
       expect(init(options));
@@ -26,65 +35,83 @@ describe('init module', () => {
   });
 
   test('destinationDir option', () => {
-    fs.rmSync(options.destinationDir, { recursive: true, force: true });
+    const o = options.destinationDir;
     options.destinationDir = undefined;
     init(options);
     expect(fs.existsSync('mpth')).toBe(true);
-    options.destinationDir = 'test/in-out/dest';
+    options.destinationDir = o;
   });
 
-  test('templateDir option', () => {
-    fs.rmSync(options.destinationDir, { recursive: true, force: true });
-    options.templateDir = undefined;
-    init(options);
-    expect(fs.existsSync('mpth/mpth-template.pug')).toBe(true);
-    options.templateDir = 'test/in-out/dest';
-  });
-
-  test('dataOutDir option', () => {
-    fs.rmSync(options.destinationDir, { recursive: true, force: true });
-    options.dataOutDir = undefined;
-    init(options);
-    expect(fs.existsSync('mpth/mpth-data.pug')).toBe(true);
-    options.dataOutDir = 'test/in-out/dest';
-  });
-
-  test('use option', () => {
-    fs.rmSync(options.destinationDir, { recursive: true, force: true });
-    options.use = false;
-    init(options);
-    expect(
-      fs.readFileSync(
-        `${options.destinationDir}/article1/article1.html`,
-        'utf8'
-      )
-    ).toMatchSnapshot();
-    options.use = true;
-  });
-
-  describe('mpth-articles.html file with a list of articles', () => {
-    test('If the index option is not specified, then by default it is true, which means a mpth-articles.html file is generated', () => {
-      fs.rmSync(options.destinationDir, { recursive: true, force: true });
-      options.sourceDir = 'test/in-out/content';
+  describe('temlateDir option and mpth-template.pug file', () => {
+    test('templateDir not specified, mpth-template.pug file in the mpth directory', () => {
+      options.templateDir = undefined;
       init(options);
-      const list = getDataList();
       expect(
-        fs.existsSync(`${options.destinationDir}/mpth-articles.html`)
-      ).toBe(true);
+        fs.readFileSync('mpth/mpth-template.pug', 'utf8')
+      ).toMatchSnapshot();
+      fs.rmSync('mpth', { recursive: true, force: true });
     });
 
-    test('If the index option is set to false, then a mpth-articles.html is not generated', () => {
-      fs.rmSync(options.destinationDir, { recursive: true, force: true });
+    test('templateDir specified, mpth-template.pug file in the test/in-out/dest directory', () => {
+      options.templateDir = 'test/in-out/dest';
+      init(options);
+      expect(
+        fs.readFileSync('test/in-out/dest/mpth-template.pug', 'utf8')
+      ).toMatchSnapshot();
+    });
+  });
+
+  describe('dataOutDir option and mpth-data.pug file', () => {
+    test('dataOutDir not specified, mpth-data.pug file in the mpth directory', () => {
+      options.dataOutDir = undefined;
+      init(options);
+      expect(fs.existsSync('mpth/mpth-data.pug')).toBe(true);
+      fs.rmSync('mpth', { recursive: true, force: true });
+    });
+
+    test('dataOutDir specified, mpth-data.pug file in the test/in-out/dest directory', () => {
+      options.dataOutDir = 'test/in-out/dest';
+      init(options);
+      expect(
+        fs.readFileSync('test/in-out/dest/mpth-data.pug', 'utf8')
+      ).toMatchSnapshot();
+    });
+  });
+
+  describe('use option', () => {
+    test('use is specified false, the template is not used', () => {
+      options.use = false;
+      init(options);
+      expect(
+        fs.readFileSync(
+          `${options.destinationDir}/article1/article1.html`,
+          'utf8'
+        )
+      ).toMatchSnapshot();
+    });
+
+    test('use is specified true (by default), the template is used', () => {
+      options.use = true;
+      init(options);
+      expect(
+        fs.readFileSync(
+          `${options.destinationDir}/article1/article1.html`,
+          'utf8'
+        )
+      ).toMatchSnapshot();
+    });
+  });
+
+  describe('index option and mpth-articles.html file', () => {
+    test('index is specified false, the mpth-articles.html is not generated', () => {
       options.index = false;
       init(options);
-      const list = getDataList();
       expect(
         fs.existsSync(`${options.destinationDir}/mpth-articles.html`)
       ).toBe(false);
     });
 
-    test('Writing to the mpth-articles.html file is correct', () => {
-      fs.rmSync(options.destinationDir, { recursive: true, force: true });
+    test('index is specified true (by default), the mpth-articles.html is generated', () => {
       options.index = true;
       init(options);
       expect(
@@ -93,29 +120,8 @@ describe('init module', () => {
     });
   });
 
-  describe('mpth-data.pug data file', () => {
-    test('Writing to the mpth-data.pug file is correct', () => {
-      fs.rmSync(options.destinationDir, { recursive: true, force: true });
-      init(options);
-      expect(
-        fs.readFileSync(`${options.dataOutDir}/mpth-data.pug`, 'utf8')
-      ).toMatchSnapshot();
-    });
-  });
-
-  describe('mpth-template.pug template file', () => {
-    test('Writing to the mpth-template.pug file is correct', () => {
-      fs.rmSync(options.destinationDir, { recursive: true, force: true });
-      init(options);
-      expect(
-        fs.readFileSync(`${options.templateDir}/mpth-template.pug`, 'utf8')
-      ).toMatchSnapshot();
-    });
-  });
-
-  describe('Creating a description', () => {
+  describe('description property', () => {
     test('Description of the article does not exist, so it is created from the first paragraph', () => {
-      fs.rmSync(options.destinationDir, { recursive: true, force: true });
       init(options);
       const lists = getDataList();
       expect(lists[2].description).toBe('Paragraph of the third article');
@@ -124,7 +130,6 @@ describe('init module', () => {
 
   describe('Files not with the .md extension', () => {
     test('The no-md-file.txt file is copied to the dest/article1 directory', () => {
-      fs.rmSync(options.destinationDir, { recursive: true, force: true });
       init(options);
       expect(
         fs.existsSync(`${options.destinationDir}/article1/no-md-file.txt`)
@@ -134,17 +139,9 @@ describe('init module', () => {
 });
 
 describe('getDataList module', () => {
-  fs.rmSync(options.destinationDir, { recursive: true, force: true });
-  init(options);
-  const lists = getDataList();
-
   test('In the "list" array, the first object contains the "title" property with the content "Title of the first article"', () => {
-    expect(lists[0].title).toBe('Title of the first article');
-  });
-
-  test('In the "list" array, the second object contains the "description" property with the content "Brief description of the second article"', () => {
-    expect(lists[1].description).toBe(
-      'Brief description of the second article'
-    );
+    init(options);
+    const lists = getDataList();
+    expect(lists).toMatchSnapshot();
   });
 });
